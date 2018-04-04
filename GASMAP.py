48ab15d2aa2d6ec911f2b64254525d6c8bffeb9d
Damith Senanayake <dracusds123@gmail.com>
2018-04-04 12:09:25 +1000

    windows transit

--- GASMAP.py
+++ GASMAP.py
@@ -23,7 +23,7 @@ class GASMAP(object):
     def fit_transform(self, X, colors):
         intdim = 3
 
-        g_max = 25
+        g_max = 10000
 
         W = np.random.random((intdim, X.shape[1]))
         Y = np.random.random((intdim, 2))
@@ -36,24 +36,30 @@ class GASMAP(object):
 
         hits = np.zeros(G.shape[0])
 
-        lrst = 0.1
-        alpha = 0.02
-        maxiter = 20
+        lrst = 0.5
+        alpha = 0.01
+        maxiter = 200
         D = X.shape[1]
-        sf = 0.8
-        GT = -np.log(sf) * D
 
+        QE = []
+        NG = []
+        GTs = []
         print 'Graph Dimensionality : ', intdim
-
         for i in range(maxiter):
-            a_max = 2  # (a_max_st*(1 - i * 1. / maxiter))
+            a_max = 2  # -(i%2==0)# (a_max_st*(1 - i * 1. / maxiter))
 
-            struct_change = (i % 5 == 1)
-
+            sf = 0.1
+            GT = -np.log(sf) * X.shape[1] * np.exp(-7.5 * (1. * i) ** 6 / maxiter ** 6)
+            GTs.append(GT)
+            QE.append(errors.sum())
+            NG.append(G.shape[0])
+            struct_change = (i % 15 == 0)
+            errors.fill(0)
             for x in X:
                 print '\r iteration : ', i, ' : n(G) : ', G.shape[0],
                 dists = np.linalg.norm(W - x, axis=1)
                 candidates = np.argsort(dists)[:intdim]
+
                 #
                 # if i%4 == 0:
                 # t, u = np.argsort(np.linalg.norm(W-W[s], axis=1))[1:3]
@@ -81,7 +87,6 @@ class GASMAP(object):
 
                 # Move Y
 
-
                 ages[s] += G[s]
                 ages[s][s] = 0
                 # if not( i%4 == 0) :
@@ -105,65 +110,50 @@ class GASMAP(object):
                             ages[u][t] = 0
                             ages[t][u] = 0
 
-                # if i >= 0.1 * maxiter:
-                #     # z_d = (d - d.mean()) / d.std()
-                #     z_D = (D - D.mean()) / D.std()
-                #
-                #     del_edges = np.where(z_D>2.)[0]#np.where((z_d > 3.) | (z_D >1.))[0]  # 3.5 *(np.exp(-1.*(i)**2/(0.5*maxiter)*2)))[0]
-                #
-                #     G[s][del_edges] = 0
-                #     G[:, s][del_edges] = 0
-                #     ages[s][del_edges] = 0
-                #     ages[:, s][del_edges] = 0
+            if struct_change:
+                print ' creating new node, ',
+                while errors.max() > GT ** 2:
+                    grix = np.where(errors > GT ** 2)[0]
+                    for s in range(G.shape[0]):
+                        if errors[s] > GT ** 2:  # and gens[s] < g_max:
+                            try:
+                                ninds = np.where(G[s] == 1)[0]
+                                h_er_ixs = ninds[np.argsort(errors[ninds])][:intdim]
+                            except ValueError:
+                                continue
+                            W_n = np.sum(W[h_er_ixs], axis=0) + W[s]
+                            W_n /= intdim * 1.
 
-                # if not G[u][t]:
+                            W_n = 2 * W[s] - W_n
 
-                #
-                # if i% 100 == 51 :
-                #     neidists = np.linalg.norm(W[s]-W[neis], axis=1)
-                #     del_node = np.argmax(neidists)
-                #
-                #     G[s][neis[del_node]]=0
-                #     G[:, s][neis[del_node]]=0
+                            Y_n = Y[h_er_ixs].sum(axis=0) + Y[s]
+                            Y_n /= intdim * 1.
 
+                            Y_n = 2 * Y[s] - Y_n
 
-                if struct_change:
-                    print ' creating new node, ',
-                    if errors[s] > GT ** 2 and gens[s] < g_max:
-                        try:
-                            ninds = np.where(G[s] == 1)[0]
-                            h_er_ixs = ninds[np.argsort(errors[ninds])][:intdim]
-                        except ValueError:
-                            continue
-                        W_n = np.sum(W[h_er_ixs], axis=0) + W[s]
-                        W_n /= intdim * 1.
+                            errors[h_er_ixs] *= alpha
+                            errors[s] *= alpha
+                            gens[s] += 1
+                            gens[h_er_ixs] += 1
+                            W = np.concatenate((W, np.array([W_n])), axis=0)
+                            Y = np.concatenate((Y, np.array([Y_n])), axis=0)
+                            G = np.concatenate((G, np.array([np.zeros(G.shape[0])])), axis=0)
+                            G = np.concatenate((G, np.array([np.zeros(G.shape[0])]).T), axis=1)
+                            G[s][-1] = 1
+                            G[-1][s] = 1
+                            G[h_er_ixs][:, -1] = 1
+                            G[:, -1][h_er_ixs] = 1
+                            ages = np.concatenate((ages, np.array([np.zeros(ages.shape[0])])), axis=0)
+                            ages = np.concatenate((ages, np.array([np.zeros(ages.shape[0])]).T), axis=1)
 
-                        W_n = 2 * W[s] - W_n
+                            errors = np.append(errors, errors[s])
+                            hits = np.append(hits, 0)
+                            gens = np.append(gens, 0)
 
-                        Y_n = Y[h_er_ixs].sum(axis=0) + Y[s]
-                        Y_n /= intdim * 1.
-
-                        Y_n = 2 * Y[s] - Y_n
-
-                        errors[h_er_ixs] *= alpha
-                        errors[s] *= alpha
-                        gens[s] += 1
-                        gens[h_er_ixs] += 1
-                        W = np.concatenate((W, np.array([W_n])), axis=0)
-                        Y = np.concatenate((Y, np.array([Y_n])), axis=0)
-                        G = np.concatenate((G, np.array([np.zeros(G.shape[0])])), axis=0)
-                        G = np.concatenate((G, np.array([np.zeros(G.shape[0])]).T), axis=1)
-                        G[s][-1] = 1
-                        G[-1][s] = 1
-                        ages = np.concatenate((ages, np.array([np.zeros(ages.shape[0])])), axis=0)
-                        ages = np.concatenate((ages, np.array([np.zeros(ages.shape[0])]).T), axis=1)
-                        errors = np.append(errors, errors[s])
-                        hits = np.append(hits, 0)
-                        gens = np.append(gens, 0)
-
-                        # move y
-            move_range = 1
-            if struct_change: move_range = 20
+                            # move y
+            move_range = 10
+            if struct_change: move_range = 50 * (i ** 2 / maxiter ** 2)
+            if i + 1 >= maxiter: move_range = 200
             print ' moving all nodes in graph, ',
             for _ in range(move_range):
                 for p in range(Y.shape[0]):
@@ -179,17 +169,17 @@ class GASMAP(object):
                     # pushdirs = np.array([np.exp(-d_oths)]).T  * 250
                     pushdirs = np.array([self.t_dist(d_oths)]).T  # * 5
                     # pushdirs /= pushdirs.min()
-                    # pushdirs /= pushdirs.sum()
+                    pushdirs /= pushdirs.sum()
 
                     push = (Y[oths] - Y[p]) * pushdirs
 
                     Y[oths] += push  # * lr
 
-                    pulldirs = np.array([(d[y_neis])]).T
+                    pulldirs = np.array([(d[y_neis])]).T  # **2
                     if pulldirs.sum():
                         pulldirs /= pulldirs.sum()
 
-                    Y[p] += 0.01 * ((Y[y_neis] - Y[p]) * pulldirs).sum(axis=0)  # *lr
+                    Y[p] += 0.4 * ((Y[y_neis] - Y[p]) * pulldirs).sum(axis=0)  # *lr
 
             if struct_change or i == maxiter - 1:
                 emptyNodes = np.where((G.sum(axis=0) <= intdim - 2))  # | ( hits<=1))
@@ -204,44 +194,6 @@ class GASMAP(object):
                     hits = np.delete(hits, emptyNodes)
                     hits.fill(0)
                     emptyNodes = np.where((G.sum(axis=0) <= intdim - 2))  # | ( hits<=1))
-
-        # Map Correction
-        #
-        # fig = plt.figure()
-        # ax = fig.add_subplot(111, projection='3d')
-        # ax.scatter(X.T[0], X.T[1],X.T[2], c=colors, cmap=plt.cm.hsv, alpha=0.2)
-        # ax.scatter(W.T[0], W.T[1],W.T[2], c='black', alpha=0.8)
-        #
-        #
-        # for i in range(G.shape[0]):
-        #     for j in range(G.shape[1]):
-        #         if G[i, j]:
-        #             ax.plot([W[i, 0], W[j, 0]], [W[i, 1], W[j, 1]],[W[i,2], W[j,2]], c='black')
-        #
-        # plt.show()
-
-        print '\n'
-        for _ in range(1000):
-            for p in range(Y.shape[0]):
-                print '\rcorrecting:, ', _,
-                y_neis = np.where(G[p] == 1)[0]
-
-                oths = np.where(G[p] == 0)[0]  # np.array(range(G.shape[0]))#
-                d = np.linalg.norm(Y - Y[p], axis=1)
-                d_oths = d[oths]  # np.linalg.norm(Y[oths] - Y[p], axis=1)
-                # d_oths = d_oths/d_oths.sum()
-                # pushdirs = np.array([np.exp(-d_oths)]).T * 25
-                pushdirs = np.array([self.t_dist(d_oths)]).T  # * 25
-
-                pulldirs = np.array([(d[y_neis])]).T
-                if pulldirs.sum():
-                    pulldirs /= pulldirs.sum()
-
-                push = (Y[oths] - Y[p]) * pushdirs * np.exp(-.5 * _ ** 2 / 1000. ** 2)  # *0.2
-
-                Y[oths] += push
-
-                Y[p] += 0.1 * ((Y[y_neis] - Y[p]) * pulldirs).sum(axis=0)
 
         fig1 = plt.figure()
         for i in range(G.shape[0]):
